550e36d6ee438683f5d4c0492bb674e93af832a1
Eric Huey <erichuey@Erics-MacBook-Pro.local>
2019-10-28 23:09:46 -0700

    complete

--- SQL_HW.sql
+++ SQL_HW.sql
@@ -65,10 +65,123 @@ FROM payment
 INNER JOIN staff ON staff.staff_id
 GROUP BY staff.first_name;
 
-select *
+select title, COUNT(inventory_id) as 'num copies'
 FROM film
-WHERE title = "Hunchback Impossible";
+INNER JOIN inventory
+using (film_id)
+WHERE title = "Hunchback Impossible"
+Group By title;
 
-SELECT 
-    
-	
+SELECT * FROM payment;
+
+SELECT * FROM customer;
+
+SELECT c.first_name, c.last_name, SUM(p.amount) as paid_total
+FROM payment as p
+INNER JOIN customer AS c
+ON p.customer_id = c.customer_id
+GROUP BY c.customer_id
+ORDER BY c.last_name;
+
+SELECT title
+FROM film
+WHERE title LIKE 'K%' OR title LIKE 'Q%'
+AND language_id IN
+(
+	SELECT language_id
+    FROM language
+    WHERE name = 'English'
+    );
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN
+(
+	SELECT actor_id
+    FROM film_actor
+    WHERE film_id = 
+    (
+		SELECT film_id
+        FROM film
+        WHERE title = 'Alone Trip'
+        )
+	);
+
+SELECT first_name, last_name, email, country
+FROM customer as cust
+INNER JOIN address as a
+ON (cust.address_id = a.address_id)
+INNER JOIN city 
+ON (a.city_id = city.city_id)
+INNER JOIN country as ctry
+ON (city.country_id = ctry.country_id)
+WHERE ctry.country = 'canada';
+
+SELECT title, cat.name
+FROM film
+INNER JOIN film_category as fc
+ON (film.film_id = fc.film_id)
+INNER JOIN category cat
+ON (cat.category_id = fc.category_id)
+WHERE name = 'family';
+
+SELECT title, COUNT(title) as 'Num Rentals'
+FROM film as f
+INNER JOIN inventory as i
+ON (f.film_id = i.film_id)
+INNER JOIN rental as r
+ON (i.inventory_id = r.inventory_id)
+GROUP BY title
+ORDER BY 'Num Rentals' desc;
+
+SELECT s.store_id, SUM(amount) AS 'gross revenue'
+FROM payment p
+INNER JOIN rental r
+ON (p.rental_id = r.rental_id)
+INNER JOIN inventory i
+ON (i.inventory_id = r.inventory_id)
+INNER JOIN store s
+ON (s.store_id = i.store_id)
+GROUP BY s.store_id;
+
+SELECT store_id, city, country
+FROM store s
+INNER JOIN address a
+ON (s.address_id = a.address_id)
+INNER JOIN city
+ON (city.city_id = a.city_id)
+INNER JOIN country ctry
+ON (city.country_id = ctry.country_id);
+
+SELECT SUM(amount) as 'Total Sales', cat.name as 'Genre'
+FROM payment p
+INNER JOIN rental r
+ON (p.rental_id = r.rental_id)
+INNER JOIN inventory i
+ON (r.inventory_id = i.inventory_id)
+INNER JOIN film_category fc
+on (i.film_id = fc.film_id)
+INNER JOIN category cat
+ON (fc.category_id = cat.category_id)
+GROUP BY cat.name
+ORDER BY SUM(amount) DESC
+LIMIT 5;	
+
+CREATE VIEW top_five_genres AS
+SELECT SUM(amount) as 'Total Sales', cat.name as 'Genre'
+FROM payment p
+INNER JOIN rental r
+ON (p.rental_id = r.rental_id)
+INNER JOIN inventory i
+ON (r.inventory_id = i.inventory_id)
+INNER JOIN film_category fc
+on (i.film_id = fc.film_id)
+INNER JOIN category cat
+ON (fc.category_id = cat.category_id)
+GROUP BY cat.name
+ORDER BY SUM(amount) DESC
+LIMIT 5;	
+
+SELECT * FROM top_five_genres;
+
+DROP VIEW top_five_genres;
